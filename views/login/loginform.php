ab6a428c06df0d1b118eb0cb7a615db5a7cc09f3
Jaap-Willem Dooge <jaap-willem.dooge@outlook.com>
2013-05-25 20:24:20 +0200

    Basic CSS

--- views/login/loginform.php
+++ views/login/loginform.php
@@ -1,1 +1,6 @@
-Please login:
+<div class="dialog">
+	<div class="dialogtitle">Please login</div>
+	<div class="dialogcontent">
+	bla
+	</div>
+</div>
--- views/system/header.php
+++ views/system/header.php
@@ -3,5 +3,50 @@
 <head>
 	<title><?php echo($this->settings->title); ?> :: <?php echo($this->pagetitle); ?></title>
 	<meta charset="UTF-16">
+	<style>
+		*{
+			font-family: Tahoma, Verdana, Arial;
+			font-size: 12px;
+			padding: 0px;
+			margin: 0px;
+			-webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
+			-moz-box-sizing: border-box;    /* Firefox, other Gecko */
+			box-sizing: border-box;         /* Opera/IE 8+ */
+		}
+		
+		.topbar{
+			width: 100%;
+			height: 35px;
+			font-size: 20px;
+			padding: 5px;
+			margin-bottom: 10px;
+			color: #fff;
+			background-color: #000;
+		}
+		
+		.content{
+			width: 100%;
+			padding: 2px;
+		}
+		
+		.dialog{
+			margin: 0px auto;
+			width: 500px;
+			border: 1px #000 solid;
+		}
+		.dialogtitle{
+			width: 100%;
+			background-color: #000;
+			color: #fff;
+			height: 30px;
+			font-size: 16px;
+			padding: 5px;
+		}
+		.dialogcontent{
+			padding: 5px;
+		}
+	</style>
 </head>
 <body>
+<div class="topbar"><?php echo($this->settings->title); ?> :: <?php echo($this->pagetitle); ?></div>
+<div class="content">
