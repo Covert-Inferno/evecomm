add1528f0c0b1f5bdf8600e892e2ad16b3971475
Jaap-Willem Dooge <jaap-willem.dooge@outlook.com>
2013-07-03 11:48:04 +0200

    Moved most settings to the database and renamed adminchar to requestchar for 3rd party requests.

--- settings.php
+++ settings.php
@@ -6,21 +6,9 @@ class Settings{
 	// Set it to (for example) your alliance name.
 	public $title = 'EVEComm';
 	
-	// This is an array of AllianceID's that are allowed to register.
-	// If you want to ues EVEComm for a single corporation, leave the array empty.
-	// To find out your AllianceID, search for it in this list:
-	// https://api.eveonline.com/eve/AllianceList.xml.aspx
-	public $alliancewhitelist = array('1');
-	
-	// This is an array of CorporationID's that are allowed to register.
-	// Corporations that are in a whitelisted alliance don't need to be added here.
-	// To find out your CorporationID, use an account API key to connect to the following URL:
-	// https://api.eveonline.com/account/Characters.xml.aspx?keyID=<insert your key id here>&vCode=<insert your verification code here>
-	public $corporationwhitelist = array('1');
-	
-	// Admin character name.
+	// Request character name.
 	// This is used in (for example) the request URL's to eve-marketdata.com so they can contact you in case of issues and shown to users for technical support.
-	public $adminchar = 'demo';
+	public $requestchar = 'demo';
 	
 	// MySQL login details.
 	// Please set these values to connect to the MySQL database.
@@ -28,16 +16,5 @@ class Settings{
 	public $db_user = 'evecomm';	//username
 	public $db_pass = 'evecomm';	//user's password (make sure it is not 'evecomm', set it to something secure!)
 	public $db_name = 'evecomm';	//database name
-	
-	// Here you can enable or disable modules - if you write own additions, it is prefered to load them here.
-	// Module order also affects the menu order.
-	// The default modules are: news, boards, fleettool, lootlog, messaging, wallet (in this order)
-	public $enabled_modules = array('news', 'boards', 'fleettool', 'lootlog', 'messaging', 'wallet');
-	
-	// Here you can enable or disable module widgets to show in the title bar.
-	// Module order also affects the widget order - keep in mind that not all modules have widgets.
-	// Modules that are not loaded will be ignored.
-	// The default widgets are: messaging, wallet (in this order)
-	public $enabled_widgets = array('messaging', 'wallet');
 	
 }
